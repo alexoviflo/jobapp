da2102564f09415ad37615798a063873a9eac8d0
alexoviflo <alexander.ovesen@online.no>
2024-09-14 16:54:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './layout/app-layout'
+import LandingPage from './pages/landing'
+import Onboarding from './pages/onboarding'
+import JobListing from './pages/job-listing'
+import JobPage from './pages/job'
+import PostJob from './pages/post-job'
+import SavedJobs from './pages/saved-job'
+import MyJobs from './pages/my-jobs'
+import { ThemeProvider } from "@/components/theme-provider"
+
+const router = createBrowserRouter([
+  {
+    element:<AppLayout />,
+    children: [
+   {   path: '/',
+       element: <LandingPage /> 
+   },
+   {   path: '/onboarding',
+    element: <Onboarding /> 
+    }
+    ,
+   {   path: '/jobs',
+    element: <JobListing /> 
+    }
+    ,
+   {   path: '/job/:id',
+    element: <JobPage /> 
+    }
+    ,
+   {   path: '/post-job',
+    element: <PostJob /> 
+    }
+    ,
+   {   path: '/saved-job',
+    element: <SavedJobs /> 
+    }
+    ,
+   {   path: '/my-jobs',
+    element: <MyJobs /> 
+    }
+
+    ] 
+  }
+])
+
+function App() {
+
+  return <div>
+
+   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+  <RouterProvider router={router}/>
+    </ThemeProvider>
+   
+  </div>
+}
+
+export default App
